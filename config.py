ec4926d1daf1a788f0d1ac15e67d4005f63b8a95
Hieu Chu <minhieuchu@gmail.com>
2025-02-13 10:10:55 +0700

    Add Dockerfile & Update docker-compose.yml

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 
-SERVER_HOST = os.getenv("SERVER_HOST", "localhost")
-SERVER_PORT = os.getenv("SERVER_PORT", 8080)
+SERVER_HOST = os.getenv("SERVER_HOST", "0.0.0.0")
+SERVER_PORT = int(os.getenv("SERVER_PORT", 8080))
 
 MONGODB_HOST = os.getenv("MONGODB_HOST", "localhost")
-MONGODB_PORT = os.getenv("MONGODB_PORT", 27017)
+MONGODB_PORT = int(os.getenv("MONGODB_PORT", 27017))
 MONGODB_DATABASE = os.getenv("MONGODB_DATABASE", "algo_trial")
 MONGODB_DATABASE_TEST = os.getenv("MONGODB_DATABASE_TEST", "algo_trial_test")
